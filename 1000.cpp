54059d1aafa97939aa56cd3fed28df85cbc5eadb
shikshik99 <chldustlr07@gmail.com>
2019-10-04 18:28:57 +0900

    C++

--- 1000.cpp
+++ 1000.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 int main(void) {
 	int a, b;
+	
 	cin >> a >> b;
 	cout << a + b << endl;
+
+	return 0;
 }
--- 1065.cpp
+++ 1065.cpp
@@ -2,9 +2,6 @@
 using namespace std;
 
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-
 	int N, cnt = 0;
 	int a, b, c;
 
