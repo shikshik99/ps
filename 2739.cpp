a691482f569794b8d690fb7ea067934547a485ee
shikshik99 <chldustlr07@gmail.com>
2019-11-04 20:40:05 +0900

    C++

---
+++ 2739.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int gugu;
+	cin >> gugu;
+	for (int i = 1; i < 10; i++)
+		cout << gugu << " * " << i << " = " << gugu * i << endl;
+
+	return 0;
+}
