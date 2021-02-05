88a8741f0bd813fb686be8731faa2b2c91b08b7b
shikshik99 <chldustlr07@gmail.com>
2021-02-05 17:18:28 +0900

    2021-02-05

--- string/1225.cpp
+++ string/1225.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
+#define ll long long
 string str1, str2;
 
 int main(){
     cin >> str1 >> str2;
-    int ans = 0;
+    ll ans = 0;
     for(int i = 0; i < str1.length(); i++){
         for(int j = 0; j < str2.length(); j++){
             ans += (str1[i] - '0') * (str2[j] - '0');
