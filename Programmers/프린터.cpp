c660564e4e4d88cad57c1de11828a11fa2243c12
shikshik99 <chldustlr07@gmail.com>
2021-02-05 15:54:08 +0900

    2021-02-05

---
+++ Programmers/프린터.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> priorities, int location) {
+    int answer = 0;
+    queue <pair<int,int>>q;
+    priority_queue <int>pq;
+    for(int i = 0; i < priorities.size(); i++){pq.push(priorities[i]); q.push(make_pair(i, priorities[i]));}
+    
+    pair<int,int> tmp;
+    while(!q.empty()){
+        if(q.front().second == pq.top() && q.front().first == location){break;}
+        else if(q.front().second == pq.top()) {q.pop(); pq.pop();answer++;}
+        else {tmp = q.front(); q.pop(); q.push(tmp);}
+    }
+    
+    return answer + 1;
+}
