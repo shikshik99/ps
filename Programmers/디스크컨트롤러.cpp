abf1d43c40fc334abf868cf27563376bc9df2fe8
shikshik99 <chldustlr07@gmail.com>
2021-02-07 23:41:11 +0900

    2021-02-07

---
+++ Programmers/디스크컨트롤러.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <queue>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+struct cmp{
+    bool operator()(pair<int,int>a, pair<int,int>b){
+    return a.first > b.first ? true : false;
+    }
+};
+
+priority_queue <pair<int,int>, vector<pair<int,int>>, cmp>pq;
+vector <int> ans;
+
+int solution(vector<vector<int>> jobs) {
+    int answer = 0, time = 0, i = 0;
+    sort(jobs.begin(), jobs.end());
+    
+    while(1){
+        if(pq.empty()) {if(time < jobs[i][0]) time = jobs[i][0]; pq.push({jobs[i][1], jobs[i][0]}), i++;}
+        while(i < jobs.size() && jobs[i][0] <= time) pq.push({jobs[i][1], jobs[i][0]}), i++;
+        time += pq.top().first, ans.push_back(time - pq.top().second), pq.pop();
+        if(i >= jobs.size()) break;
+    }
+    while(!pq.empty()) time +   = pq.top().first, ans.push_back(time - pq.top().second), pq.pop();
+    
+    for(auto it : ans) answer += it;
+    return answer / ans.size();
+}
