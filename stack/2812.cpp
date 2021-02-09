02ec39ba933b8ea3119490eab74fb3300c5adf42
shikshik99 <chldustlr07@gmail.com>
2021-02-09 16:31:16 +0900

    2021-02-09

--- stack/2812.cpp
+++ stack/2812.cpp
@@ -1,31 +1,20 @@
 #include <iostream>
-#include <stack>
 #include <string>
+#include <stack>
 using namespace std;
-#define ll long long
 
-ll arr[10] = {0};
-ll tmp[10] = {0};
-string str;
 stack <char>s;
-ll N, K, cnt = 0;
+string str;
 
 int main(){
-    cin >> N >> K >> str;
-    for(ll i = 0; i < str.size(); i++) arr[str[i]-'0']++;
-    for(ll i = 0; i < 10; i++){
-        cnt += arr[i];
-        if(cnt > K) {
-            cnt -= arr[i], tmp[i] = K - cnt; break;
-        }
-        else if(cnt == K) {tmp[i] = arr[i]; break;}
-        else tmp[i] = arr[i];
-    }
-    for(ll i = 0; i < str.size(); i++) {
-        if(tmp[str[i]-'0']) tmp[str[i]-'0']--;
-        else s.push(str[i]);
-    }
-    str.clear();
+     int N, K;
+     cin >> N >> K >> str;
+
+     for(int i = 0; i < str.length(); i++){
+         while(K && !s.empty() && s.top() - '0' < str[i] - '0') s.pop(), K--;
+         s.push(str[i]);             
+     }
+     str.clear();
     while(!s.empty()) str += s.top(), s.pop();
-    for(ll i = str.size() - 1; i >= 0; i--) cout << str[i];
+    for(int i = str.length() - 1; i >= K; i--) cout << str[i];
 }
