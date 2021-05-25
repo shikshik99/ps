61001c8606a9069421df5738833d703005d66e95
shikshik99 <chldustlr07@gmail.com>
2021-05-25 18:11:56 +0900

    2021-05-25

---
+++ BOJ/DP/15992.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+#define mod 1000000009
+
+long long d[1001][1001];
+
+int foo(int num, int cnt){
+    if(!num && !cnt) return 1;
+    if(num < 0 || cnt < 0) return 0;
+    
+    long long &ret = d[num][cnt];
+    if(ret != -1) return ret;
+
+    ret = 0;
+    for(int i = 1; i <= 3; i++){
+        ret = (ret + foo(num-i, cnt - 1)) % mod;
+    }
+    
+    return ret;
+}
+
+int main(){
+    memset(d, -1, sizeof(d));
+    int T, n, m;
+    cin >> T;
+    while(T--){
+        cin >> n >> m;
+        cout << foo(n, m) << '\n';
+    }
+}
