76d84fd174c783b62b94a83182b98e23da6154a8
shikshik99 <chldustlr07@gmail.com>
2021-07-07 14:52:11 +0900

    2021-07-07

--- BOJ/1713.cpp
+++ BOJ/1713.cpp
@@ -2,39 +2,64 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-#define pii pair<int, int>
 
-vector <pii> v;
-int rc[101];
-vector <int> answer;
+struct student{
+    int recom = 0;
+    int duration = 0;
+};
 
-int main(){
-    int n, m, tmp;
-    pii mi;
-    bool flag;
-    cin >> n >> m;
-    v.resize(n, {-1, 0});
-    rc[0] = 0x7f7f7f7f;
-    for(int i = 0; i < m; i++){
-        for(int j = 0; j < n; j++) v[j].second++;
-        flag = false;
-        cin >> tmp;
-        mi.first = 0;
-        mi.second = 0x7f7f7f7f;
-        for(int j = 0; j < n; j++){
-            if(v[j].first == tmp) {rc[tmp]++, flag = true; break;}
-            else if(v[j].first == -1) {rc[tmp]++, v[j].first = tmp, v[j].second = 0, flag = true; break;}
-            else{
-                if(rc[mi.first] > rc[v[j].first]) mi.first = v[j].first, mi.second = v[j].second;
-                else if(rc[mi.first] == rc[v[j].first]) {if(mi.second <= v[j].second) mi.first = v[j].first, mi.second = v[j].second;}
-            }
-        }
-        if(flag) continue;
-        for(int j = 0; j < n; j++){
-            if(v[j].first == mi.first) {rc[mi.first] = 0, v[j].first = tmp, rc[tmp]++, v[j].second = 0; break;}
+vector <student> v;
+int arr[21] = {0,};
+int N, K;
+
+bool chk_exist(int tmp){
+    for(int i = 0; i < N; i++){
+        if(arr[i] == tmp) {
+            v[tmp].recom++; 
+            return true;
         }
     }
-    for(auto it : v) answer.push_back(it.first);
+
+    return false;
+}
+
+void find_place(int new_idx){
+    if(chk_exist(new_idx)) return;
+    
+    v[new_idx].recom = 1;
+
+    int cnt = 10000, du = 0, tmp, idx, ii;
+    for(int i = 0; i < N; i++){
+        if(!arr[i]) {
+            arr[i] = new_idx;
+            return;
+        }
+        tmp = arr[i];
+        if(v[tmp].recom < cnt) cnt = v[tmp].recom, du = v[tmp].duration, idx = tmp, ii = i;
+        else if(v[tmp].recom == cnt && v[tmp].duration > du) cnt = v[tmp].recom, du = v[tmp].duration, idx = tmp, ii = i;
+    }
+    v[idx].duration = 0, v[idx].recom = 0;
+    arr[ii] = new_idx;
+}
+
+int main(){
+    v.resize(101);
+
+    int tmp;
+    cin >> N >> K;
+    
+    for(int i = 0; i < K; i++){
+        cin >> tmp;
+        find_place(tmp);
+        for(int i = 0; i < N; i++) {
+            v[arr[i]].duration++;
+        }
+    }
+    
+    vector <int> answer;
+    for(int i = 0; i < N; i++){
+        answer.push_back(arr[i]);
+    }
     sort(answer.begin(), answer.end());
     for(auto it : answer) cout << it << ' ';
 }
