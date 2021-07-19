3ee7b3dcb24e852d7211e0e4b354bc7f45e1c5c5
shikshik99 <chldustlr07@gmail.com>
2021-07-19 13:36:21 +0900

    2021-07-19

--- BOJ/implementation/3425.cpp
+++ BOJ/implementation/3425.cpp
@@ -1,181 +1,216 @@
 #include <iostream>
 #include <stack>
-#include <vector>
 #include <string>
+#include <vector>
 #include <cmath>
 using namespace std;
 #define MAX 1000000000
+using ll = long long;
 
-stack <int> s;
+stack <ll> s;
 vector <string> cmd;
-vector <int> num;
+vector <ll> num;
 
-void clear_stack(){
-    while(!s.empty()) s.pop();
+void NUM(ll tmp){
+    s.push(tmp);
 }
 
-bool MOD(){
-    if(s.size() < 2) {cout << "ERROR\n"; return false;}
+bool POP(){
+    if(s.empty()) {
+        cout << "ERROR\n";
+        return false;
+    }
 
-    int a, b;
-    bool f = true;
-    a = s.top(), s.pop(), b = s.top(), s.pop();
-    
-    if(!a) {cout << "ERROR\n"; return false;}
-
-    if(b < 0) f = false;
-
-    b = abs(b) % abs(a);
-    if(!f) b *= -1;
-    s.push(b);
+    s.pop();
 
     return true;
-    
 }
 
-bool DIV(){
-    if(s.size() < 2) {cout << "ERROR\n"; return false;}
-    
-    int a, b;
-    bool f = true;
-    a = s.top(), s.pop(), b = s.top(), s.pop();
-    
-    if(!a) {cout << "ERROR\n"; return false;}
+bool INV(){
+    if(s.empty()) {
+        cout << "ERROR\n";
+        return false;
+    }
 
-    if(a < 0) f ^= true;
-    if(b < 0) f ^= true;
+    ll tmp = s.top(); s.pop();
+    s.push(tmp * -1);
 
-    b = abs(b) / abs(a);
-    if(!f) b *= -1;
-    s.push(b);
+    return true;
+}
+
+bool DUP(){
+    if(s.empty()) {
+        cout << "ERROR\n";
+        return false;
+    }
+
+    s.push(s.top());
+
+    return true;
+}
+
+bool SWP(){
+    if(s.size() < 2) {
+        cout << "ERROR\n";
+        return false;
+    }
+
+    ll A, B;
+    A = s.top(), s.pop(), B = s.top(), s.pop();
+    s.push(A), s.push(B);
+
+    return true;
+}
+
+bool ADD(){
+    if(s.size() < 2) {
+        cout << "ERROR\n";
+        return false;
+    }
+
+    ll A, B;
+    A = s.top(), s.pop(), B = s.top(), s.pop();
+
+    if(abs(A + B) > MAX){
+        cout << "ERROR\n";
+        return false;
+    }
+
+    s.push(A + B);
+
+    return true;
+}
+
+bool SUB(){
+    if(s.size() < 2) {
+        cout << "ERROR\n";
+        return false;
+    }
+
+    ll A, B;
+    A = s.top(), s.pop(), B = s.top(), s.pop();
+
+    if(abs(B - A) > MAX){
+        cout << "ERROR\n";
+        return false;
+    }
+
+    s.push(B - A);
 
     return true;
 }
 
 bool MUL(){
-    if(s.size() < 2) {cout << "ERROR\n"; return false;}
-    
-    long long a, b;
-    a = s.top(), s.pop(), b = s.top(), s.pop();
-
-    if(abs(a * b) > MAX) {cout << "ERROR\n"; return false;}
-
-    s.push(a * b);
-    return true;
-}
-
-bool SUB(){
-    if(s.size() < 2) {cout << "ERROR\n"; return false;}
-    
-    int a, b;
-    a = s.top(), s.pop(), b = s.top(), s.pop();
-    
-    if(abs(b - a) > MAX) {cout << "ERROR\n"; return false;}
-    
-    s.push(b - a);
-    return true;
-}
-
-bool ADD(){
-    if(s.size() < 2) {cout << "ERROR\n"; return false;}
-
-    int a, b;
-    a = s.top(), s.pop(), b = s.top(), s.pop();
-    
-    if(abs(a + b) > MAX) {cout << "ERROR\n"; return false;}
-
-    s.push(a + b);
-    return true;
-}
-
-bool SWP(){
-    if(s.size() < 2) {cout << "ERROR\n"; return false;}
-    
-    int a, b;
-    a = s.top(), s.pop(), b = s.top(), s.pop();
-    s.push(a), s.push(b);
-    return true;
-}
-
-bool DUP(){
-    if(s.empty()) {cout << "ERROR\n"; return false;}
-    s.push(s.top());
-    return true;
-}
-
-bool INV(){
-    if(s.empty()) {cout << "ERROR\n"; return false;}
-    int tmp = s.top();
-    s.pop(); s.push(tmp * -1);
-    
-    return true;
-}
-
-bool POP(){
-    if(s.empty()) {cout << "ERROR\n"; return false;}
-    s.pop();
-    return true;
-}
-
-void NUM(int tmp){
-    s.push(tmp);
-}
-
-void foo(int v){
-    clear_stack();
-    s.push(v);
-
-    int num_idx = 0, len = cmd.size();
-    for(int i = 0; i < len; i++){
-        if(cmd[i] == "NUM"){
-            NUM(num[num_idx]), num_idx++;
-        }
-        else if(cmd[i] == "POP"){
-            if (!POP()) return;
-        }
-        else if(cmd[i] == "INV"){
-            if (!INV()) return;
-        }
-        else if(cmd[i] == "DUP"){
-            if (!DUP()) return;
-        }
-        else if(cmd[i] == "SWP"){
-            if(!SWP()) return;
-        }
-        else if(cmd[i] == "ADD"){
-            if(!ADD()) return;
-        }
-        else if(cmd[i] == "SUB"){
-            if(!SUB()) return;
-        }
-        else if(cmd[i] == "MUL"){
-            if(!MUL()) return;
-        }
-        else if(cmd[i] == "DIV"){
-            if(!DIV()) return;
-        }
-        else if(cmd[i] == "MOD"){
-            if(!MOD()) return;
-        }
+    if(s.size() < 2) {
+        cout << "ERROR\n";
+        return false;
     }
-    if(s.size() != 1) cout << "ERROR\n";
-    else cout << s.top() << '\n', clear_stack();
+
+    ll A, B;
+    A = s.top(), s.pop(), B = s.top(), s.pop();
+
+    if(abs(A * B) > MAX){
+        cout << "ERROR\n";
+        return false;
+    }
+
+    s.push(A * B);
+
+    return true;
+}
+
+bool DIV(){
+    if(s.size() < 2) {
+        cout << "ERROR\n";
+        return false;
+    }
+
+    bool f = true;
+    ll A, B;
+    A = s.top(), s.pop(), B = s.top(), s.pop();
+
+    if(!A){
+        cout << "ERROR\n";
+        return false;
+    }
+
+    if(A < 0) f ^= true;
+    if(B < 0) f ^= true;
+
+    if(f) s.push(abs(B) / abs(A));
+    else s.push((abs(B) / abs(A)) * -1);
+
+    return true;
+}
+
+bool MOD(){
+    if(s.size() < 2) {
+        cout << "ERROR\n";
+        return false;
+    }
+
+    bool f = true;
+    ll A, B;
+    A = s.top(), s.pop(), B = s.top(), s.pop();
+
+    if(!A){
+        cout << "ERROR\n";
+        return false;
+    }
+
+    if(B < 0) f ^= true;
+
+    if(f) s.push(abs(B) % abs(A));
+    else s.push((abs(B) % abs(A)) * -1);
+
+    return true;
+}
+
+void CLEAR(){
+    while(!s.empty()) s.pop();
+    cmd.clear(), num.clear();
+}
+
+void foo(){
+    int N, tmp, idx;
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        while(!s.empty()) s.pop();
+
+        bool flag = true;
+        idx = 0;
+        cin >> tmp;
+        s.push(tmp);
+        for(int j = 0; j < cmd.size(); j++){
+            if(cmd[j] == "NUM") NUM(num[idx++]);
+            if(cmd[j] == "POP") flag = POP();
+            if(cmd[j] == "INV") flag = INV();
+            if(cmd[j] == "DUP") flag = DUP();
+            if(cmd[j] == "SWP") flag = SWP();
+            if(cmd[j] == "ADD") flag = ADD();
+            if(cmd[j] == "SUB") flag = SUB();
+            if(cmd[j] == "MUL") flag = MUL();
+            if(cmd[j] == "DIV") flag = DIV();
+            if(cmd[j] == "MOD") flag = MOD();
+
+            if(!flag) break;
+        }
+        if(!flag) continue;
+        if(s.size() != 1) cout << "ERROR\n";
+        else cout << s.top() << '\n';
+    }
+    CLEAR();
 }
 
 int main(){
-    string str = "";
-    int N, tmp;
-
-    while(str != "QUIT"){
-        cin >> str;
-        if(str == "END"){
-            cin >> N;
-            for(int i = 0; i < N; i++) cin >> tmp, foo(tmp);
-            cmd.clear(), num.clear();
-            clear_stack();
-            cout << '\n';
+    string tmp = "";
+    while(tmp != "QUIT"){
+        cin >> tmp;
+        if(tmp == "END") {
+            foo(), cout << '\n';
+            continue;
         }
-        else if(str == "NUM") cmd.push_back(str), cin >> str, num.push_back(stoi(str));
-        else cmd.push_back(str);
+        cmd.push_back(tmp);
+        if(tmp == "NUM") cin >> tmp, num.push_back(stoll(tmp));
     }
 }
