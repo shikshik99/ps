d87d88fc0b9f644e58459017a6895a41fdaf6605
shikshik99 <chldustlr07@gmail.com>
2021-07-19 20:25:53 +0900

    2021-07-19

--- BOJ/greedy/1339.cpp
+++ BOJ/greedy/1339.cpp
@@ -1,51 +1,39 @@
 #include <iostream>
-#include <map>
 #include <vector>
 #include <string>
 #include <algorithm>
 using namespace std;
+using ll = long long;
 
-map <char, int> m;
-vector <string> v;
-vector <char> tmp;
+vector <ll> v;
 int N;
 
-int calc(){
-    int ret = 0, temp = 0;
-    for(int i = 0; i < N; i++){
-        temp = 0;
-        for(int j = 0; j < v[i].length(); j++){
-            temp = (temp * 10) + m[v[i][j]];
-        }
-        ret += temp;
+void add_alpha(string str){
+    for(int i = 0; i < str.length(); i++){
+        int num = str.length() - i - 1;
+        int tmp = 1;
+        while(num--) tmp *= 10;
+        v[str[i] - 'A'] += tmp;
     }
-
-    return ret;
 }
 
 int main(){
-    for(int i = 0; i < 26; i++) m[i + 'A'] = 0;
     cin >> N;
     string str;
-    for(int i = 0; i < N; i++) {
-        cin >> str, v.push_back(str);
-        for(int j = 0; j < str.length(); j++){
-            tmp.push_back(str[j]);
-        }
+
+    v.resize(26, 0);
+    for(int i = 0; i < N; i++){
+        cin >> str;
+        add_alpha(str);
     }
+    sort(v.begin(), v.end(), greater<>());
 
-    sort(tmp.begin(), tmp.end());
-    tmp.erase(unique(tmp.begin(), tmp.end()), tmp.end());
-
-    int answer = 0;
-    do{
-        int cnt = 9;
-        for(int i = 0; i < tmp.size(); i++){
-            m[tmp[i]] = cnt;
-            cnt--;
-        }
-        answer = max(answer, calc());
-    }while(next_permutation(tmp.begin(), tmp.end()));
-
+    int i = 0, j = 9;
+    ll answer = 0;
+    while(v[i]){
+        answer += (v[i] * j);
+        i++, j--;
+    }
+    
     cout << answer << '\n';
 }
