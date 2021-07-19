b053a186478b09e26da12208b720c400873b34a6
shikshik99 <chldustlr07@gmail.com>
2021-07-19 15:48:46 +0900

    2021-07-19

--- BOJ/DFS/1103.cpp
+++ BOJ/DFS/1103.cpp
@@ -3,42 +3,41 @@
 #include <algorithm>
 using namespace std;
 
-int N, M;
-int board[51][51], D[51][51];
-int arr[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+int arr[4][2] = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
 bool chk[51][51];
+char board[51][51];
+int D[51][51];
+int N, M;
 
 int DFS(int x, int y){
-    if(D[x][y] != -1) return D[x][y];
+    int &ret = D[x][y];
+    if(ret != -1) return ret;
 
-    D[x][y] = 1;
+    ret = 1;
+    
     chk[x][y] = true;
     for(int i = 0; i < 4; i++){
-        int dx = x + (arr[i][0] * board[x][y]);
-        int dy = y + (arr[i][1] * board[x][y]);
-        if(dx < 0 || dx >= N || dy < 0 || dy >= M || board[dx][dy] == -1) continue;
+        int num = (board[x][y] - '0');
+        int dx = x + (num * arr[i][0]);
+        int dy = y + (num * arr[i][1]);
+        if(dx < 0 || dx >= N || dy < 0 || dy >= M || board[dx][dy] == 'H') continue;
         if(chk[dx][dy]) cout << -1, exit(0);
-        D[x][y] = max(D[x][y], DFS(dx, dy) + 1);
+        ret = max(ret, DFS(dx, dy) + 1);
     }
-
     chk[x][y] = false;
-    return D[x][y];
+
+    return ret;
 }
 
 int main(){
-    memset(chk, false, sizeof(chk));
-    memset(D, -1, sizeof(D));
-
-    char ch;
-
     cin >> N >> M;
     for(int i = 0; i < N; i++){
         for(int j = 0; j < M; j++){
-            cin >> ch;
-            if(ch == 'H') board[i][j] = -1;
-            else board[i][j] = (ch - '0');
+            cin >> board[i][j];
         }
     }
-    
-    cout << DFS(0, 0);
+
+    memset(chk, false, sizeof(chk));
+    memset(D, -1, sizeof(D));
+    cout << DFS(0,0) << '\n';
 }
