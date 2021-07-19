7391bb530a5fc921d516e1190865897c58b2c572
shikshik99 <chldustlr07@gmail.com>
2021-07-19 20:02:14 +0900

    2021-07-19

--- BOJ/back tracking/2580.cpp
+++ BOJ/back tracking/2580.cpp
@@ -1,48 +1,54 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-#define pii pair <int, int>
+using pii = pair<int, int>;
 
 int board[9][9];
 vector <pii> v;
+int sizz;
 
-void print_board(){
+bool OK(int idx){
+    int x = v[idx].first;
+    int y = v[idx].second;
     for(int i = 0; i < 9; i++){
-        for(int j = 0; j < 9; j++) cout << board[i][j] << ' ';
-        cout << '\n';
+        if(board[x][y] == board[x][i] && i != y) return false;
+        if(board[x][y] == board[i][y] && i != x) return false;
     }
-}
 
-bool ok(int x, int y){
-    for(int i = 0; i < 9; i++){
-        if(i != x && board[i][y] == board[x][y]) return false;
-        if(i != y && board[x][i] == board[x][y]) return false;
-    }
-    int dx = (x / 3) * 3, dy = (y / 3) * 3;
+    int dx = (x / 3) * 3;
+    int dy = (y / 3) * 3;
     for(int i = dx; i < dx + 3; i++){
         for(int j = dy; j < dy + 3; j++){
             if(i == x && j == y) continue;
-            if(board[i][j] == board[x][y]) return false;       
+            if(board[i][j] == board[x][y]) return false;
         }
     }
 
     return true;
 }
 
+void print_board(){
+    for(int i = 0; i < 9; i++){
+        for(int j = 0; j < 9; j++){
+            cout << board[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+}
+
 void foo(int idx){
-    if(idx == v.size()) print_board(), exit(0);
-    int x = v[idx].first, y = v[idx].second;
+    if(idx == sizz) print_board(), exit(0);
+    
     for(int i = 1; i < 10; i++){
+        int x = v[idx].first;
+        int y = v[idx].second;
         board[x][y] = i;
-        if(ok(x, y)) foo(idx + 1);
+        if(OK(idx)) foo(idx + 1);
         board[x][y] = 0;
     }
 }
 
 int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0), cout.tie(0);
-
     for(int i = 0; i < 9; i++){
         for(int j = 0; j < 9; j++){
             cin >> board[i][j];
@@ -50,5 +56,6 @@ int main(){
         }
     }
 
+    sizz = v.size();
     foo(0);
 }
