3cfa61aacdf8bd665dcd327b5bc70fe78175035a
shikshik99 <chldustlr07@gmail.com>
2021-07-19 17:47:47 +0900

    2021-07-19

--- BOJ/back tracking/9663.cpp
+++ BOJ/back tracking/9663.cpp
@@ -5,27 +5,29 @@ using namespace std;
 int arr[16];
 int N;
 
-bool ok(int depth){
+bool chk(int depth){
     for(int i = 0; i < depth; i++){
-        if(arr[depth] == arr[i]) return false;
-        if(abs(depth - i) == abs(arr[depth] - arr[i])) return false;
+        if(arr[i] == arr[depth]) return false;
+        if(abs(i - depth) == abs(arr[i] - arr[depth])) return false;
     }
-    
+
     return true;
 }
 
 int foo(int depth){
     if(depth == N) return 1;
     int answer = 0;
+    
     for(int i = 0; i < N; i++){
         arr[depth] = i;
-        if(ok(depth)) answer += foo(depth + 1);
+        if(chk(depth)) answer += foo(depth + 1);
     }
-    
+
     return answer;
 }
 
 int main(){
+    for(int i = 0; i < 16; i++) arr[i] = -1;
     cin >> N;
     cout << foo(0);
 }
