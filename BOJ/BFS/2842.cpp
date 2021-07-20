034485332d6c949f86c12f98b756e68a161ccef8
shikshik99 <chldustlr07@gmail.com>
2021-07-20 18:31:47 +0900

    2021-07-20

--- BOJ/BFS/2842.cpp
+++ BOJ/BFS/2842.cpp
@@ -1,81 +1,79 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <queue>
 #include <cstring>
+#include <queue>
 using namespace std;
-#define pii pair<int, int>
-
-bool chk[51][51];
-char board[51][51];
-int height_board[51][51];
-int dir[9][2] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 0}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+using pii = pair <int, int>;
 
 vector <pii> house;
-vector <int> height;
-int post_x, post_y, N;
+vector <int> v;
+int dir[8][2] = {{-1, 0}, {1, 0}, {-1, 1}, {1, 1}, {-1, -1}, {1, -1}, {0, -1}, {0, 1}};
+int height[51][51];
+char board[51][51];
+bool chk[51][51];
+int N, PX, PY;
 
-bool chk_board(){
-    int sizz = house.size();
-    for(int i = 0; i < sizz; i++){
-        pii tmp = house[i];
-        if(!chk[tmp.first][tmp.second]) return false;
+bool chk_house(){
+    int len = house.size();
+    for(int i = 0; i < len; i++){
+        int dx = house[i].first;
+        int dy = house[i].second;
+        if(!chk[dx][dy]) return false;
     }
 
     return true;
 }
 
-bool BFS(int start, int end){
-    int max_height = height[end], min_height = height[start];
-    if(height_board[post_x][post_y] > max_height || height_board[post_x][post_y] < min_height) return false;
+bool BFS(int MIN, int MAX){
+    if(height[PX][PY] < MIN || height[PX][PY] > MAX) return false;
 
     memset(chk, false, sizeof(chk));
     queue <pii> q;
-    q.push({post_x, post_y});
-    chk[post_x][post_y] = true;
+    q.push({PX, PY});
+    chk[PX][PY] = true;
     
     while(!q.empty()){
         pii tmp = q.front(); q.pop();
-        for(int i = 0; i < 9; i++){
+        for(int i = 0; i < 8; i++){
             int dx = tmp.first + dir[i][0];
             int dy = tmp.second + dir[i][1];
-            if(dx < 0 || dx >= N || dy < 0 || dy >= N || chk[dx][dy] || height_board[dx][dy] < min_height || height_board[dx][dy] > max_height) continue;
-            q.push({dx, dy});
+            if(dx < 0 || dx >= N || dy < 0 || dy >= N || chk[dx][dy] || height[dx][dy] < MIN || height[dx][dy] > MAX) continue;
             chk[dx][dy] = true;
+            q.push({dx, dy});
         }
     }
-    if(chk_board()) return true;
-    return false;
+    return chk_house();
 }
 
 int main(){
     cin >> N;
     for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
+        for(int j = 0; j < N; j++) {
             cin >> board[i][j];
-            if(board[i][j] == 'P') post_x = i, post_y = j;
+            if(board[i][j] == 'P') PX = i, PY = j;
             if(board[i][j] == 'K') house.push_back({i, j});
         }
     }
 
     for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            cin >> height_board[i][j];
-            height.push_back(height_board[i][j]);
+        for(int j = 0; j < N; j++) {
+            cin >> height[i][j];
+            v.push_back(height[i][j]);
         }
     }
 
-    sort(height.begin(), height.end());
-    height.erase(unique(height.begin(), height.end()), height.end());
+    sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end());
 
-    int answer = 0x7f7f7f7f;
-    int start = 0, end = 0, sizz = height.size();
-    while(end < sizz){
-        while(BFS(start, end)){
-            answer = min(answer, (height[end] - height[start]));
-            start++;
+    int len = v.size();
+    int l = 0, r = 0, answer = 0x6f6f6f6f;
+    while(r <= len){
+        while(BFS(v[l], v[r])){
+            answer = min(answer, (v[r] - v[l]));
+            l++;
         }
-        end++;
+        r++;
     }
 
     cout << answer << '\n';
