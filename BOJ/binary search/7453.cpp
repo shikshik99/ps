c2161b4ae33b2ecb14957cf30e796740af2d00f6
shikshik99 <chldustlr07@gmail.com>
2021-07-20 18:07:48 +0900

    2021-07-20

--- BOJ/binary search/7453.cpp
+++ BOJ/binary search/7453.cpp
@@ -2,36 +2,34 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-#define ll long long
+using ll = long long;
 
-vector <ll> A, B, C, D;
 vector <ll> v1, v2;
-int N;
 
 int main(){
+    int N;
     cin >> N;
-    A.resize(N), B.resize(N), C.resize(N), D.resize(N);
-    for(int i = 0; i < N; i++) cin >> A[i] >> B[i] >> C[i] >> D[i];
-
-    ll sum1, sum2;
+    vector <ll> A(N), B(N), C(N), D(N);
+    for(int i = 0; i < N; i++){
+        cin >> A[i] >> B[i] >> C[i] >> D[i];
+    }
     for(int i = 0; i < N; i++){
         for(int j = 0; j < N; j++){
-            sum1 = A[i] + B[j];
-            sum2 = C[i] + D[j];
-            v1.push_back(sum1), v2.push_back(sum2);
+            v1.push_back(A[i] + B[j]);
+            v2.push_back(C[i] + D[j]);
         }
-    }
+    }   
 
+    sort(v1.begin(), v1.end());
     sort(v2.begin(), v2.end());
-
-    ll answer = 0, tmp;
-    ll len = v1.size(), lo, hi;
-    for(int i = 0; i < len; i++){
-        tmp = 0 - v1[i];
-        lo = lower_bound(v2.begin(), v2.end(), tmp) - v2.begin();
-        hi = upper_bound(v2.begin(), v2.end(), tmp) - v2.begin();
+    ll answer = 0, target;
+    int idx = 0, lo, hi;
+    while(idx < v1.size()){
+        target = 0 - v1[idx++];
+        lo = lower_bound(v2.begin(), v2.end(), target) - v2.begin();
+        hi = upper_bound(v2.begin(), v2.end(), target) - v2.begin();
         answer += (hi - lo);
     }
-
+    
     cout << answer << '\n';
 }
