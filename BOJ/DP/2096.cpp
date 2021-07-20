6f840f89b2f5e5e828abd377a5f0feb40762bb73
shikshik99 <chldustlr07@gmail.com>
2021-07-20 17:26:49 +0900

    2021-07-20

--- BOJ/DP/2096.cpp
+++ BOJ/DP/2096.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
+using ll = long long;
 
-int D[6] = {0, };
+ll D[6] = {0,};
+int A, B, C;
+ll max0, max1, max2, min0, min1, min2;
 
 int main(){
     int N;
-    int tmp1, tmp2, tmp3;
-    int max1 = 0, max2 = 0, max3 = 0;
-    int min1 = 0, min2 = 0, min3 = 0;
-
     cin >> N;
-    for(int i = 0; i < N; i++){
-        cin >> tmp1 >> tmp2 >> tmp3;
-        max1 = max(D[0], D[1]) + tmp1;
-        max2 = max({D[0], D[1], D[2]}) + tmp2;
-        max3 = max(D[1], D[2]) + tmp3;
-        min1 = min(D[3], D[4]) + tmp1;
-        min2 = min({D[3], D[4], D[5]}) + tmp2;
-        min3 = min(D[4], D[5]) + tmp3;
-        D[0] = max1, D[1] = max2, D[2] = max3;
-        D[3] = min1, D[4] = min2, D[5] = min3;
+    while(N--){
+        cin >> A >> B >> C;
+        max0 = max(D[0], D[1]) + A;
+        max1 = max({D[0], D[1], D[2]}) + B;
+        max2 = max(D[1], D[2]) + C;
+        min0 = min(D[3], D[4]) + A;
+        min1 = min({D[3], D[4], D[5]}) + B;
+        min2 = min(D[4], D[5]) + C;
+        D[0] = max0, D[1] = max1, D[2] = max2, 
+        D[3] = min0, D[4] = min1, D[5] = min2;
     }
 
     cout << max({D[0], D[1], D[2]}) << ' ' << min({D[3], D[4], D[5]}) << '\n';
