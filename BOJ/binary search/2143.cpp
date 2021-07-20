fdc05a419c717941e983eb0951beb8c9e9bb4957
shikshik99 <chldustlr07@gmail.com>
2021-07-20 17:40:23 +0900

    2021-07-20

--- BOJ/binary search/2143.cpp
+++ BOJ/binary search/2143.cpp
@@ -2,41 +2,50 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-#define ll long long
+using ll = long long;
 
 vector <ll> A, B;
-vector <ll> arr1, arr2;
-int T, N, M;
-
-void subarray(int idx, int depth, int max, ll sum, vector <ll> &tmp, vector <ll> origin){
-    for(int i = idx; i < max; i++){
-        sum += origin[i];
-        tmp.push_back(sum);
-    }
-}
+vector <ll> v1, v2;
+int T, n, m;
 
 int main(){
-    cin >> T >> N;
-    arr1.resize(N);
-    for(int i = 0; i < N; i++) cin >> arr1[i];
-    cin >> M;
-    arr2.resize(M);
-    for(int i = 0; i < M; i++) cin >> arr2[i];
+    cin >> T >> n;
+    A.resize(n, 0);
+    for(int i = 0; i < n; i++) cin >> A[i];
+    cin >> m;
+    B.resize(m, 0);
+    for(int i = 0; i < m; i++) cin >> B[i];
 
-    for(int i = 0; i < N; i++) subarray(i, 0, N, 0, A, arr1);
-    for(int i = 0; i < M; i++) subarray(i, 0, M, 0, B, arr2);
-    
-    
-    sort(A.begin(), A.end());
-    sort(B.begin(), B.end());
-    
-    ll answer = 0, target;
-    int lo, hi;
-    for(int i = 0; i < A.size(); i++){
-        target = T - A[i];
-        lo = lower_bound(B.begin(), B.end(), target) - B.begin();
-        hi = upper_bound(B.begin(), B.end(), target) - B.begin();
-        answer += hi - lo;
+    ll sum = 0;
+    for(int i = 0; i < n; i++){
+        v1.push_back(A[i]);
+        sum = A[i];
+        for(int j = i + 1; j < n; j++){
+            sum += A[j];
+            v1.push_back(sum);
+        }
     }
+
+    for(int i = 0; i < m; i++){
+        v2.push_back(B[i]);
+        sum = B[i];
+        for(int j = i + 1; j < m; j++){
+            sum += B[j];
+            v2.push_back(sum);
+        }
+    }
+
+    sort(v1.begin(), v1.end());
+    sort(v2.begin(), v2.end());
+
+    ll target, answer = 0;
+    int idx = 0, lo, hi;
+    while(idx < v1.size()){
+        target = T - v1[idx++];
+        lo = lower_bound(v2.begin(), v2.end(), target) - v2.begin();
+        hi = upper_bound(v2.begin(), v2.end(), target) - v2.begin();
+        answer += (hi - lo);
+    }
+
     cout << answer << '\n';
 }
