ba9fc3ef8ce83b1c26f648eb391205b97b47565e
shikshik99 <chldustlr07@gmail.com>
2021-07-21 16:35:27 +0900

    2021-07-21

--- BOJ/data structure/queue/10845.cpp
+++ BOJ/data structure/queue/10845.cpp
@@ -4,46 +4,46 @@
 using namespace std;
 
 deque <int> dq;
+int N;
 
-void PUSH(int num){
-    dq.push_back(num);
-}
-
-void POP(){
-    if(dq.empty()) cout << -1 << '\n';
-    else cout << dq.front() << '\n', dq.pop_front();
-}
-
-void SIZE(){
-    cout << dq.size() << '\n';
-}
-
-void EMPTY(){
-    if(dq.empty()) cout << 1 << '\n';
-    else cout << 0 << '\n';
-}
-
-void FRONT(){
-    if(dq.empty()) cout << -1 << '\n';
-    else cout << dq.front() << '\n';
-}
-
-void BACK(){
-    if(dq.empty()) cout << -1 << '\n';
-    else cout << dq.back() << '\n';
+void cmd(int f, int num){
+    switch(f){
+        case 0 :
+            dq.push_back(num);
+            break;
+        case 1 :
+            if(dq.empty()) cout << -1 << '\n';
+            else cout << dq.front() << '\n', dq.pop_front();
+            break;
+        case 2 :
+            cout << dq.size() << '\n';
+            break;
+        case 3 :
+            cout << dq.empty() << '\n';
+            break;
+        case 4 :
+            if(dq.empty()) cout << -1 << '\n';
+            else cout << dq.front() << '\n';
+            break;
+        case 5 :
+            if(dq.empty()) cout << -1 << '\n';
+            else cout << dq.back() << '\n';
+            break;
+    }
 }
 
 int main(){
-    string str, num;
-    int N;
     cin >> N;
+    string str;
+    int f, num;
     while(N--){
         cin >> str;
-        if(str == "push") cin >> num, PUSH(stoi(num));
-        else if(str == "pop") POP();
-        else if(str == "size") SIZE();
-        else if(str == "empty") EMPTY();
-        else if(str == "front") FRONT();
-        else if(str == "back") BACK();
+        if(str == "push") f = 0, cin >> num;
+        else if(str == "pop") f = 1;
+        else if(str == "size") f = 2;
+        else if(str == "empty") f = 3;
+        else if(str == "front") f = 4;
+        else if(str == "back") f = 5;
+        cmd(f, num);
     }
 }
