f544b70a102e607f0feac3d762c34bf900d6b87c
shikshik99 <chldustlr07@gmail.com>
2021-07-23 13:35:11 +0900

    2021-07-23

---
+++ BOJ/math/1837.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <cmath>
+#define MAX 1000000
+using namespace std;
+using pii = pair<int, int>;
+
+bool chk[MAX + 1];
+string P;
+int K;
+
+void chk_prime(){
+    memset(chk, false, sizeof(chk));
+    int len = sqrt(MAX);
+    for(int i = 2; i <= len; i++){
+        if(chk[i]) continue;
+        for(int j = i * 2; j <= MAX; j += i) chk[j] = true;
+    }
+}
+
+bool divide(int num){
+    int len = P.length();
+    int tmp = 0;
+    for(int i = 0; i < len; i++){
+        tmp += (P[i] - '0');
+        tmp %= num;
+        tmp *= 10;
+    }
+    if(!tmp) return true;
+    return false;
+}
+
+int foo(){
+    int ret = 0x6f6f6f6f;
+    for(int i = 2; i <= MAX; i++){
+        if(chk[i]) continue;
+        if(divide(i)) {
+            ret = min(ret, i);
+        }
+    }
+    return ret;
+}
+
+int main(){
+    cin >> P >> K;
+    chk_prime();
+    int answer = foo();
+    if(answer < K) cout << "BAD " << answer << '\n';
+    else cout << "GOOD\n";
+}
