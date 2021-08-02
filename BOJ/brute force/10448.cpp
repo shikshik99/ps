f40703ad54023406ab6fc2179a014f75479725fb
shikshik99 <chldustlr07@gmail.com>
2021-08-03 01:53:45 +0900

    2021-08-03

--- BOJ/brute force/10448.cpp
+++ BOJ/brute force/10448.cpp
@@ -1,30 +1,27 @@
 #include <iostream>
-#include <vector>
+#define MAX 46
 using namespace std;
 
-vector <int> v;
-vector <int> in;
-vector <int> answer;
+int arr[MAX];
+int T, K;
 
-void foo(int num){
-    for(int a = 0; a < 45; a++){
-        for(int b = 0; b < 45; b++){
-            for(int c = 0; c < 45; c++){
-                if(v[a]+v[b]+v[c] == num) {answer.push_back(1); return;}
+int solve(){
+    for(int i = 1; i < MAX; i++){
+        for(int j = 1; j < MAX; j++){
+            for(int k = 1; k < MAX; k++){
+                if(arr[i] + arr[j] + arr[k] == K) return 1;
             }
         }
     }
-    answer.push_back(0);
+    
+    return 0;
 }
 
 int main(){
-    int N;
-    cin >> N;
-    for(int i = 1; i < 46; i++) v.push_back(i*(i+1)/2);
-    in.resize(N);
-    for(int i = 0; i < N; i++) {
-        cin >> in[i];
-        foo(in[i]);
+    cin >> T;
+    for(int i = 1; i < MAX; i++) arr[i] = (i * (i + 1)) / 2;
+    while(T--){
+        cin >> K;
+        cout << solve() << '\n';
     }
-    for(auto it : answer) cout << it << '\n';
 }
