5ae40ad93b837b97e53ae78ceb27ef1114fefa81
shikshik99 <chldustlr07@gmail.com>
2021-08-06 19:46:13 +0900

    2021-08-06

--- BOJ/greedy/4796.cpp
+++ BOJ/greedy/4796.cpp
@@ -1,19 +1,14 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-vector <int>answer;
-
 int main(){
-    int L, P , V, cnt;
+    int L, P, V = 1; // P일중 L일 동안만 사용 가능 휴가 기간은 V
+    int idx = 1, ans;
     cin >> L >> P >> V;
-    while(L && P && V){
-        cnt = (V/P)*L, V%=P;
-        if(V >= L) cnt += L;
-        else cnt += V;
-        answer.push_back(cnt);
+    while(L || P || V){
+        ans = (V / P) * L, V %= P;
+        ans += (V >= L ? L : V);
+        cout << "Case " << idx++ << ": " << ans << '\n';
         cin >> L >> P >> V;
     }
-    int i = 1;
-    for(auto it : answer) cout << "Case " << i << ": " << it << '\n', i++;
 }
