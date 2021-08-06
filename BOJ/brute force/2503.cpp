69d6a06b5ff27c3643744cdcaa9da33d689fac5e
shikshik99 <chldustlr07@gmail.com>
2021-08-06 17:41:29 +0900

    2021-08-06

--- BOJ/brute force/2503.cpp
+++ BOJ/brute force/2503.cpp
@@ -1,37 +1,51 @@
-#include <vector>
 #include <iostream>
-#include <string>
 #include <cstring>
 using namespace std;
 
-vector <string> v;
-bool chk[3];
+bool chk[10][10][10];
+int arr[10][10][10][2];
+int N;
 
 int main(){
-    int N, strike, ball, answer = 0;
-    int cnt1, cnt2;
-    string num;
-    char a, b, c;
+    memset(chk, true, sizeof(chk));
+
     cin >> N;
-    for(int i = 111; i < 1000; i++) v.push_back(to_string(i));
-    for(int i = 0; i < N; i++) {
+    int num, strike, ball;
+    int a, b, c;
+    bool x, y, z;
+    while(N--){
+        memset(arr, 0, sizeof(arr));
         cin >> num >> strike >> ball;
-        a = num[0], b = num[1], c = num[2];
-        for(int j = 0; j < v.size(); j++){
-            if(v[j] == "0") continue;
-            if(v[j][0] == '0' || v[j][1] == '0' || v[j][2] == '0'){v[j] = "0"; continue;}
-            if((v[j][0] == v[j][1]) || (v[j][0] == v[j][2]) || (v[j][1] == v[j][2])) {v[j] = "0"; continue;}
-            memset(chk, false, sizeof(chk));
-            cnt1 = 0, cnt2 = 0;
-            if(v[j][0] == a) cnt1++, chk[0] = true;
-            if(v[j][1] == b) cnt1++, chk[1] = true;
-            if(v[j][2] == c) cnt1++, chk[2] = true;
-            if(!chk[0] && (v[j][1] == a || v[j][2] == a)) cnt2++;
-            if(!chk[1] && (v[j][0] == b || v[j][2] == b)) cnt2++;
-            if(!chk[2] && (v[j][0] == c || v[j][1] == c)) cnt2++;
-            if(strike != cnt1 || ball != cnt2) v[j] = "0";
+        c = num % 10, num /= 10, b = num % 10, num /= 10, a = num;
+        for(int i = 1; i < 10; i++){
+            for(int j = 1; j < 10; j++){
+                for(int k = 1; k < 10; k++){
+                    if(!chk[i][j][k]) continue;
+                    if(i == j || i == k || j == k) {chk[i][j][k] = false; continue;}
+                    x = false, y = false, z = false;
+
+                    if(i == a) arr[i][j][k][0]++, x = true;
+                    if(j == b) arr[i][j][k][0]++, y = true;
+                    if(k == c) arr[i][j][k][0]++, z = true;
+                    if(!x && i == b || i == c) arr[i][j][k][1]++;
+                    if(!y && j == a || j == c) arr[i][j][k][1]++;
+                    if(!z && k == a || k == b) arr[i][j][k][1]++;
+
+                    if(arr[i][j][k][0] != strike || arr[i][j][k][1] != ball) chk[i][j][k] = false;
+                }
+            }
+        }
+    }   
+
+    int answer = 0 ;
+    for(int i = 1; i < 10; i++){
+        for(int j = 1; j < 10; j++){
+            for(int k = 1; k < 10; k++){
+                if(!chk[i][j][k]) continue;
+                answer++;
+            }
         }
     }
-    for(auto it : v) if(it != "0") answer++;
-    cout << answer; 
+
+    cout << answer << '\n';
 }
