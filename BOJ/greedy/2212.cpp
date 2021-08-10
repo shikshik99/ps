08cd849f06c6005ed9f6fac5ee49f60ba23eb916
shikshik99 <chldustlr07@gmail.com>
2021-08-10 20:02:44 +0900

    2021-08-10

--- BOJ/greedy/2212.cpp
+++ BOJ/greedy/2212.cpp
@@ -3,24 +3,33 @@
 #include <algorithm>
 using namespace std;
 
-vector <int> sensor;
 vector <int> v;
+vector <int> tmp;
+int N, K;
+
+bool cmp(int a, int b){
+    return a > b ? true : false;
+}
 
 int main(){
-    int N, K;
     cin >> N >> K;
-    sensor.resize(N);
-    for(int i = 0; i < N; i++) cin >> sensor[i];
-    sort(sensor.begin(), sensor.end());
-    sensor.erase(unique(sensor.begin(), sensor.end()), sensor.end());
-    for(int i = 0; i < sensor.size() - 1; i++) v.push_back(sensor[i+1] - sensor[i]);
-    sort(v.begin(), v.end(), greater<>());
-    
-    int answer = sensor[sensor.size() - 1] - sensor[0];
-    auto it = v.begin(); K--;
-    while(it != v.end() && K){
-        answer -= *it;
-        K--, it++;
+    K--;
+
+    v.resize(N);
+    for(int i = 0; i < N; i++) cin >> v[i];
+    sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end());
+
+    int len = v.size();
+    for(int i = 0; i < len - 1; i++) tmp.push_back(v[i + 1] - v[i]);
+    sort(tmp.begin(), tmp.end(), cmp);
+
+    int answer = v[len - 1] - v[0];
+    len = tmp.size();
+    for(int i = 0; i < len; i++){
+        if(!K) break;
+        answer -= tmp[i], K--;
     }
-    cout << answer;
+
+    cout << answer << '\n';
 }
