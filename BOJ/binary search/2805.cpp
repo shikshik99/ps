4ec433d1275c3fd7fc6af3da2bdb3e7ef0dd98f5
shikshik99 <chldustlr07@gmail.com>
2021-08-14 17:52:19 +0900

    2021-08-14

--- BOJ/binary search/2805.cpp
+++ BOJ/binary search/2805.cpp
@@ -1,31 +1,33 @@
 #include <iostream>
-#include <algorithm>
+#define MAX 1000001
 using namespace std;
 using ll = long long;
 
+int arr[MAX];
 int N, M;
-ll arr[1000001];
 
-ll wood(ll tmp){
+ll get_sum(int tmp){
     ll ret = 0;
-    for(int i = 0; i < N; i++){
-        if(arr[i] > tmp)
-            ret += (arr[i] - tmp);
+    for(int i = 0; i < N; i++) {
+        if(arr[i] > tmp) ret += (arr[i] - tmp);
     }
+
     return ret;
 }
 
 int main(){
-    cin >> N >> M;
-    for(int i = 0; i < N; i++) cin >> arr[i];
-    sort(arr, arr + N);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int lo = 0, hi = 0;
 
-    ll l = 0, r = arr[N - 1], mid, tmp;
-    while(l <= r){
-        mid = (l + r) / 2;
-        tmp = wood(mid);
-        if(tmp < M) r = mid - 1;
-        else l = mid + 1;
+    cin >> N >> M;
+    for(int i = 0; i < N; i++) cin >> arr[i], hi = hi > arr[i] ? hi : arr[i];
+
+    while(lo + 1 < hi){
+        int mid = (lo + hi) / 2;
+        ll sum = get_sum(mid);
+        if(sum >= M) lo = mid;
+        else hi = mid;
     }
-    cout << r << '\n';
+
+    cout << lo << '\n'; 
 }
