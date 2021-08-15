a4ef76940cab602da2b22315afd7f3875c39654e
shikshik99 <chldustlr07@gmail.com>
2021-08-15 18:29:46 +0900

    2021-08-15

--- BOJ/DP/11055.cpp
+++ BOJ/DP/11055.cpp
@@ -11,8 +11,8 @@ int foo(int idx){
     
     int &ret = D[idx];
     if(ret != -1) return ret;
-    ret = arr[idx];
 
+    ret = arr[idx];
     for(int i = idx + 1; i < N; i++){
         if(arr[idx] < arr[i]) ret = max(ret, foo(i) + arr[idx]);
     }
@@ -21,11 +21,14 @@ int foo(int idx){
 }
 
 int main(){
-    memset(D, -1, sizeof(D));
     cin >> N;
     for(int i = 0; i < N; i++) cin >> arr[i];
-    int answer = 0;
-    for(int i = 0; i < N; i++) answer = max(answer, foo(i));
 
-    cout << answer << '\n';
+    memset(D, -1, sizeof(D));
+    int answer = 0;
+    for(int i = 0; i < N; i++){
+        answer = max(answer, foo(i));
+    }
+
+    cout << answer << '\n'; 
 }
