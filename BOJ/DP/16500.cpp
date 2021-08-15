f35e4e9965d3accf8b49e6a3082b55b3d50df46e
shikshik99 <chldustlr07@gmail.com>
2021-08-15 18:25:02 +0900

    2021-08-15

--- BOJ/DP/16500.cpp
+++ BOJ/DP/16500.cpp
@@ -4,29 +4,22 @@
 #include <cstring>
 using namespace std;
 
-char D[101] = {false, };
-vector <string> v;
 string S;
+vector <string> v;
+char D[101];
 int N, len;
 
 bool foo(int idx){
+    if(idx == len) return true;
+
     char &ret = D[idx];
     if(ret != -1) return ret;
-    if(idx == len) return ret = 1;
 
-    int tmp;
     ret = 0;
     for(int i = 0; i < N; i++){
-        if(len < idx + v[i].length()) continue;
-        bool f = true;
-        tmp = idx;
-        for(int j = 0; j < v[i].length(); j++){
-            if(v[i][j] != S[tmp]) f = false;
-            tmp++;
-        }
-        if(f) ret |= foo(tmp);
-    }
-    
+        if(idx + v[i].length() > len) continue;
+        if(v[i] == S.substr(idx, v[i].length())) ret |= foo(idx + v[i].length());
+    }   
 
     return ret;
 }
@@ -34,8 +27,9 @@ bool foo(int idx){
 int main(){
     memset(D, -1, sizeof(D));
     cin >> S >> N;
-    len = S.length();
     v.resize(N);
     for(int i = 0; i < N; i++) cin >> v[i];
+
+    len = S.length();
     cout << foo(0) << '\n';
 }
