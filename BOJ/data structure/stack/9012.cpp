04b0f879f34f7f19c51baa1e4a1df695a24147e5
shikshik99 <chldustlr07@gmail.com>
2021-08-17 19:43:05 +0900

    2021-08-17

--- BOJ/data structure/stack/9012.cpp
+++ BOJ/data structure/stack/9012.cpp
@@ -1,30 +1,24 @@
 #include <iostream>
 #include <string>
-#include <vector>
-#include <stack>
 using namespace std;
 
-stack <char>s;
-vector <string>v;
-int N;
-
-void foo(){
-    for(int i = 0; i < N; i++){
-        while(!s.empty()) s.pop();
-        for(int j = 0; j < v[i].length(); j++){
-            if(v[i][j] == '(') s.push(v[i][j]);
-            else if (v[i][j] == ')' && !s.empty() && s.top() == '(') s.pop();
-            else if (v[i][j] == ')' && s.empty()) s.push(v[i][j]);
-        }
-        if(s.empty()) cout << "YES\n";
-        else cout << "NO\n";
-    }    
-}
-
 int main(){
+    int N;
+    string str;
     cin >> N;
-    cin.ignore();
-    v.resize(N);
-    for(int i = 0; i < N; i++) getline(cin, v[i]);
-    foo();
+    p :
+    while(N--){
+        cin >> str;
+        int cnt = 0;
+        for(int i = 0; i < str.length(); i++) {
+            if(str[i] == '(') cnt++;
+            else cnt--;
+            if(cnt < 0) {
+                cout << "NO\n";
+                goto p;
+            }
+        }
+        if(cnt) cout << "NO\n";
+        else cout << "YES\n";
+    }
 }
