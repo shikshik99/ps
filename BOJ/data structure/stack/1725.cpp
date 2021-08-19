7763fed54d2eda0613da98e54c18fd9d6ae332b8
shikshik99 <chldustlr07@gmail.com>
2021-08-19 20:09:27 +0900

    2021-08-19

--- BOJ/data structure/stack/1725.cpp
+++ BOJ/data structure/stack/1725.cpp
@@ -11,18 +11,20 @@ int N;
 
 int main(){
     cin >> N;
-    ll width, height, answer = 0;
     for(int i = 0; i < N; i++) cin >> H[i];
 
-    H[N] = 0;
+    H[N] = 0; // 마지막에 남은 친구들 확인하기 위한 트릭
+    ll answer = 0;
     for(int i = 0; i <= N; i++){
+        ll height, width;
         while(!s.empty() && H[s.top()] >= H[i]){
             height = H[s.top()];
             s.pop();
-            if(s.empty()) width = i;
-            else width = i - s.top() - 1; // 단순히 i - s.top()을 하면 내 이전에 있던 높은 애들 까지 셀 수가 없다.(pop 하고 남은 top이 내 전에 있던 큰 놈들 까지 센 것)
-            answer = max(answer, width * height);
+            // s.pop()을 해서 내 왼쪽에서 나보다 작은애를 찾음(왼쪽 - 오른쪽 보면서 나보다 작은 애들만 내 밑에 들어가있어서 보장된다.)
+            width = (s.empty() ? i : i - s.top() - 1); // empty라면 현재까지 내가 제일 낮았으므로 내 높이로 커버할 수 있는 너비는 현재까지 본 너비.
+            answer = max(answer, height * width);
         }
+
         s.push(i);
     }
 
