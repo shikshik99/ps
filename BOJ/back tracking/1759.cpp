5a078b982c61b327fa362c20c6e4a7769926aa55
shikshik99 <chldustlr07@gmail.com>
2021-08-23 20:41:07 +0900

    2021-08-23

--- BOJ/back tracking/1759.cpp
+++ BOJ/back tracking/1759.cpp
@@ -1,43 +1,35 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-char arr[16];
-bool chk[16];
+bool tmp[26] = {false, };
+bool chk[26] = {false, };
+bool alpha[26] = {false, };
 int L, C;
 
-bool OK(){
-    int tmp1 = 0, tmp2 = 0;
-    for(int i = 0; i < C; i++){
-        if(chk[i]){
-            if(arr[i] == 'a' || arr[i] == 'i' || arr[i] == 'e' || arr[i] == 'o' || arr[i] == 'u') tmp1++;
-            else tmp2++;
+void foo(int len, int idx, int ja, int mo){
+    if(len == L && ja >= 2 && mo >= 1){
+        for(int i = 0; i < 26; i++) {
+            if(!chk[i]) continue;
+            cout << char(i + 'a');
         }
-    }
-    
-    if(tmp1 >= 1 && tmp2 >= 2) return true;
-    return false;
-}
-
-void combination(int idx, int depth){
-    if(depth == L && OK()){
-        for(int i = 0; i < C; i++){
-            if(chk[i]) cout << arr[i];
-        }
-        cout << '\n'; return;
+        cout << '\n';
+        return;
     }
 
-    for(int i = idx; i < C; i++){
-        if(chk[i]) continue;
+    for(int i = idx + 1; i < 26; i++){
+        if(!alpha[i]) continue;
+        
         chk[i] = true;
-        combination(i + 1, depth + 1);
+        foo(len + 1, i, ja + !tmp[i], mo + tmp[i]);
         chk[i] = false;
     }
 }
 
 int main(){
     cin >> L >> C;
-    for(int i = 0; i < C; i++) cin >> arr[i];
-    sort(arr, arr + C);
-    combination(0, 0);
+    
+    tmp['a' - 'a'] = true, tmp['e' - 'a'] = true, tmp['i' - 'a'] = true, tmp['o' - 'a'] = true, tmp['u' - 'a'] = true;
+    char ch;
+    for(int i = 0; i < C; i++) cin >> ch, alpha[ch - 'a'] = true;
+    foo(0, -1, 0, 0);
 }
