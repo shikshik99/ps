df46100365566f7bc55b8b190d4e98f66cd42ff0
shikshik99 <chldustlr07@gmail.com>
2021-08-23 21:14:12 +0900

    2021-08-23

--- BOJ/back tracking/2580.cpp
+++ BOJ/back tracking/2580.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-using pii = pair<int, int>;
+using pii = pair <int, int>;
 
 int board[9][9];
 vector <pii> v;
-int sizz;
+int N;
 
-bool OK(int idx){
-    int x = v[idx].first;
-    int y = v[idx].second;
+void print_board(){
     for(int i = 0; i < 9; i++){
-        if(board[x][y] == board[x][i] && i != y) return false;
-        if(board[x][y] == board[i][y] && i != x) return false;
+        for(int j = 0; j < 9; j++){
+            cout << board[i][j] << ' ';
+        }
+        cout << '\n';
     }
+}
 
+bool possible(int x, int y, int num){
+    for(int i = 0; i < 9; i++){
+        if(i != x && board[i][y] == num) return false;
+        if(i != y && board[x][i] == num) return false;
+    }
     int dx = (x / 3) * 3;
     int dy = (y / 3) * 3;
     for(int i = dx; i < dx + 3; i++){
@@ -27,23 +33,14 @@ bool OK(int idx){
     return true;
 }
 
-void print_board(){
-    for(int i = 0; i < 9; i++){
-        for(int j = 0; j < 9; j++){
-            cout << board[i][j] << ' ';
-        }
-        cout << '\n';
-    }
-}
+void foo(int depth){
+    if(depth == N) print_board(), exit(0);
 
-void foo(int idx){
-    if(idx == sizz) print_board(), exit(0);
-    
-    for(int i = 1; i < 10; i++){
-        int x = v[idx].first;
-        int y = v[idx].second;
+    int x = v[depth].first;
+    int y = v[depth].second;
+    for(int i = 1; i <= 9; i++){
         board[x][y] = i;
-        if(OK(idx)) foo(idx + 1);
+        if(possible(x, y, i)) foo(depth + 1);
         board[x][y] = 0;
     }
 }
@@ -55,7 +52,7 @@ int main(){
             if(!board[i][j]) v.push_back({i, j});
         }
     }
-
-    sizz = v.size();
+    
+    N = v.size();
     foo(0);
 }
