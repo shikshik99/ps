62e79fadf0dd276ec6d32c6072f13d87af36016f
shikshik99 <chldustlr07@gmail.com>
2021-08-28 23:19:55 +0900

    2021-08-28

---
+++ BOJ/back tracking/15665.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int arr[9];
+int N, M;
+vector <int> v;
+vector <vector<int>> answer;
+
+void foo(int depth){
+    if(depth == M){
+       answer.push_back(v);
+       return; 
+    }
+
+    for(int i = 0; i < N; i++){
+        v.push_back(arr[i]);
+        foo(depth + 1);
+        v.pop_back();
+    }
+}
+
+int main(){
+    cin >> N >> M;
+    for(int i = 0; i < N; i++) cin >> arr[i];
+    sort(arr, arr + N);
+    
+    foo(0);
+    sort(answer.begin(), answer.end());
+    answer.erase(unique(answer.begin(), answer.end()), answer.end());
+    for(int i = 0; i < answer.size(); i++){
+        for(auto it : answer[i]) cout << it << ' ';
+        cout << '\n';
+    }
+}
