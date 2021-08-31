2f20ceaa9b52e0a939bd0466eaaa741635199cad
shikshik99 <chldustlr07@gmail.com>
2021-08-31 22:35:45 +0900

    2021-08-31

--- BOJ/data structure/priority_queue/1927.cpp
+++ BOJ/data structure/priority_queue/1927.cpp
@@ -2,67 +2,44 @@
 #define MAX 100001
 using namespace std;
 
-int arr[MAX];
-int top = 0;
+int pq[MAX] = {0, };
+int sizz = 0, top = 1;
 
-void swap(int &a, int &b){
-    int tmp = a;
-    a = b;
-    b = tmp;
-}
+void pop(){
+    if(!sizz){
+        cout << 0 << '\n'; return;
+    }
 
-bool OK(int a, int b){
-    return a > b ? true : false;
-}
-
-void PUSH(int num){
-    arr[++top] = num;
-    int tmp = top;
-    while(OK(arr[tmp / 2], arr[tmp]) && tmp > 1){
-        swap(arr[tmp], arr[tmp / 2]);
-        tmp /= 2;
+    cout << pq[top] << '\n';
+    pq[top] = pq[sizz];
+    pq[sizz--] = 0x6f6f6f6f;
+    int parent = top, child = (pq[2] < pq[3] ? 2 : 3);
+    while(parent * 2 <= sizz && pq[parent] > pq[child]){
+        swap(pq[parent], pq[child]);
+        parent = child;
+        int left = parent * 2, right = parent * 2 + 1;
+        child = (pq[left] < pq[right] ? left : right);
     }
 }
 
-int POP(){
-    if(!top) return 0;
-    if(top == 1) {
-        top--;
-        return arr[1];
+void push(int num){
+    sizz++;
+    pq[sizz] = num;
+    int parent = sizz / 2, child = sizz;
+    while(parent && pq[parent] > pq[child]){
+        swap(pq[parent], pq[child]);
+        child = parent;
+        parent = child / 2;
     }
-    int ret = arr[1];
-
-    swap(arr[1], arr[top]);
-    arr[top--] = 0x7f7f7f7f;
-    int tmp = 1;
-    bool f1 = true, f2 = true;
-    while(tmp * 2 <= top && (f1 || f2)){
-        f1 = false, f2 = false;
-        if(tmp * 2 + 1 <= top) f1 = OK(arr[tmp], arr[tmp * 2 + 1]);
-        if(tmp * 2 <= top) f2 = OK(arr[tmp], arr[tmp * 2]);
-        if(f1 && f2){
-            if(arr[tmp * 2 + 1] == arr[tmp * 2]) f1 = false;
-            else {
-                f1 = arr[tmp * 2 + 1] < arr[tmp * 2] ? true : false;
-                f2 = arr[tmp * 2] < arr[tmp * 2 + 1] ? true : false;
-            }
-        }
-        if(f1) swap(arr[tmp], arr[tmp * 2 + 1]), tmp = tmp * 2 + 1;
-        if(f2) swap(arr[tmp], arr[tmp * 2]), tmp = tmp * 2;
-    }
-
-    return ret;
 }
 
 int main(){
-    ios_base::sync_with_stdio(0), cin.tie(0);
-    int N;
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int N, num;
     cin >> N;
-    
-    int x;
     while(N--){
-        cin >> x;
-        if(!x) cout << POP() << '\n';
-        else PUSH(x);
+        cin >> num;
+        if(!num) pop();
+        else push(num);
     }
 }
