cee8d0e63811e4d1a68c0c31e8af03279474333c
shikshik99 <chldustlr07@gmail.com>
2021-09-01 15:50:28 +0900

    2021-09-01

--- BOJ/data structure/priority_queue/2014.cpp
+++ BOJ/data structure/priority_queue/2014.cpp
@@ -1,41 +1,34 @@
 #include <iostream>
 #include <queue>
 #include <set>
+#define MAX 101
 using namespace std;
 using ll = long long;
 
-struct cmp{
-    bool operator() (ll a, ll b){
-        return a > b ? true : false;
-    }
-};
-
-priority_queue <ll, vector<ll>, cmp> pq;
-set <ll> s;
-ll arr[101];
-int K, N;
-
-ll ans(){
-    ll ret = 0, ma = arr[K - 1];
-    while(N){
-        ret = pq.top(), pq.pop();
-        for(int i = 0; i < K; i++) {
-            ll tmp = ret * arr[i];
-            if(pq.size() > N && tmp > ma) continue;
-            if(s.find(tmp) != s.end()) continue;
-            ma = ma > tmp ? ma : tmp;
-            pq.push(tmp);
-            s.insert(tmp);
-        }
-        N--;
-    }
-
-    return ret;
-}
+priority_queue <ll, vector<ll>, greater<ll>> pq;
+set <ll> S;
+int arr[MAX];
+int N, K;
 
 int main(){
-    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    cin >> K >> N;
-    for(int i = 0; i < K; i++) cin >> arr[i], pq.push(arr[i]), s.insert(arr[i]);
-    cout << ans() << '\n';
+    cin >> N >> K;
+
+    for(int i = 0; i < N; i++) cin >> arr[i], pq.push(arr[i]), S.insert(arr[i]);
+
+    ll ma = arr[N - 1];
+    ll num;
+    while(K){
+        num = pq.top();
+        pq.pop();
+        for(int i = 0; i < N; i++){
+            ll tmp = num * arr[i];
+            if(pq.size() > K && tmp > ma) continue;
+            if(S.find(tmp) != S.end()) continue;
+            ma = tmp > ma ? tmp : ma;
+            pq.push(tmp), S.insert(tmp);
+        }
+        K--;
+    }
+
+    cout << num << '\n';
 }
