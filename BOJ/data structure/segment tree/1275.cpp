42d1faa294147b234328e5723483e2418322a12c
shikshik99 <chldustlr07@gmail.com>
2021-09-03 19:24:57 +0900

    2021-09-03

--- BOJ/data structure/segment tree/1275.cpp
+++ BOJ/data structure/segment tree/1275.cpp
@@ -4,25 +4,9 @@
 using namespace std;
 using ll = long long;
 
-ll arr[MAX];
+int arr[MAX];
 vector <ll> tree;
 int N, Q;
-
-void update(int start, int end, int node, int idx, ll dif){
-    if(idx < start || idx > end) return;
-    tree[node] += dif;
-    if(start == end) return;
-    int mid = (start + end) / 2;
-    update(start, mid, node * 2, idx, dif); 
-    update(mid + 1, end, node * 2 + 1, idx, dif);
-}
-
-ll query(int start, int end, int node, int left, int right){
-    if(start > right || end < left) return 0;
-    if(left <= start && end <= right) return tree[node];
-    int mid = (start + end) / 2;
-    return query(start, mid, node * 2, left, right) + query(mid + 1, end, node * 2 + 1, left, right);
-}
 
 ll init(int start, int end, int node){
     if(start == end) return tree[node] = arr[start];
@@ -30,21 +14,33 @@ ll init(int start, int end, int node){
     return tree[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
 }
 
+ll Query(int start, int end, int node, int left, int right){
+    if(start > right || end < left) return 0;
+    if(left <= start && end <= right) return tree[node];
+    int mid = (start + end) / 2;
+    return Query(start, mid, node * 2, left, right) + Query(mid + 1, end, node * 2 + 1, left, right); 
+}
+
+ll update(int start, int end, int node, int idx, int num){
+    if(idx < start || idx > end) return tree[node];
+    if(start == end) return tree[node] = num;
+    int mid = (start + end) / 2;
+    return tree[node] = update(start, mid, node * 2, idx, num) + update(mid + 1, end, node * 2 + 1, idx, num);
+}
+
 int main(){
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     cin >> N >> Q;
+
     for(int i = 0; i < N; i++) cin >> arr[i];
-
-    tree.resize(4 * N, 0);
-
-    ll x, y, a, b;
+    tree.resize(N * 4, 0);
     init(0, N - 1, 1);
+
+    int x, y, a, b;
     while(Q--){
         cin >> x >> y >> a >> b;
         if(x > y) swap(x, y);
-        cout << query(0, N - 1, 1, x - 1, y - 1) << '\n'; 
-        ll dif = (b - arr[a - 1]);
-        arr[a - 1] = b;
-        update(0, N - 1, 1, a - 1, dif);
+        cout << Query(0, N - 1, 1, x - 1, y - 1) << '\n';
+        update(0, N - 1, 1, a - 1, b);
     }
 }
