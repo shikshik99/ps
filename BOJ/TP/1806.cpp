41ca41d370033bd347d6af34bba2f9788879b42c
shikshik99 <chldustlr07@gmail.com>
2021-09-05 19:35:03 +0900

    2021-09-05

--- BOJ/TP/1806.cpp
+++ BOJ/TP/1806.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
+#define MAX 100001
+#define INF 0x6f6f6f6f
 using namespace std;
-#define MAX 0x6f6f6f6f
-using ll = long long;
 
-ll arr[100001];
+int arr[MAX];
 int N, S;
 
 int main(){
     cin >> N >> S;
     for(int i = 0; i < N; i++) cin >> arr[i];
-    int l = 0, r = 0;
-    ll sum = 0, answer = MAX;
-    while(r <= N){
-        if(sum < S) sum += arr[r++];
-        else {
-            answer = answer < (r - l) ? answer : (r - l);
-            sum -= arr[l++];
-        }
+
+    int s = 0, e = 0, answer = INF;
+    int psum = 0;
+    while(s < N){
+        if(e == N || psum > S) psum -= arr[s++];
+        else psum += arr[e++];
+        if(psum >= S) answer = answer < e - s ? answer : e - s;
     }
 
-    if(answer == MAX) cout << 0 << '\n', exit(0);
-    cout << answer << '\n';
+    if(answer == INF) cout << 0 << '\n';
+    else cout << answer << '\n';
 }
