d7d13b30d1f21a3ec2913f809a672be223f9a49d
shikshik99 <chldustlr07@gmail.com>
2021-09-06 22:29:48 +0900

    2021-09-06

--- BOJ/dijk/1753.cpp
+++ BOJ/dijk/1753.cpp
@@ -1,41 +1,61 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-using namespace std;
-#define pii pair <int, int>
+#define MAX 20001
 #define INF 0x6f6f6f6f
+using namespace std;
+using pii = pair<int, int>;
 
-vector <vector<pii>> weight;
-vector <int> D;
-priority_queue <pii> pq;
+bool chk[MAX];
+int dist[MAX];
+vector <pii> graph[MAX];
+int V, E, start;
 
-void foo(int start){
-    D[start] = 0;
-    pq.push({0, start});
+struct cmp{
+    bool operator()(pii a, pii b){
+        return a.first > b.first ? true : false;
+    }
+};
+
+void dijkstra(int s){
+    priority_queue <pii, vector<pii>, cmp> pq;
+    pq.push({0, s});
     while(!pq.empty()){
-        int now = pq.top().second;
-        int now_cost = -pq.top().first;
-        pq.pop();
-        if(D[now] < now_cost) continue;
-        for(auto it : weight[now]){
-            int next_cost = it.second + now_cost;
+        int curr;
+        do{
+            curr = pq.top().second;
+            pq.pop();
+        }while(!pq.empty() && chk[curr]);
+
+        chk[curr] = true;
+
+        for(pii it : graph[curr]){
             int next = it.first;
-            if(D[next] > next_cost){
-                D[next] = next_cost;
-                pq.push({-next_cost, next});
-            }
+            int cost = it.second + dist[curr];
+            if(cost >= dist[next]) continue;
+            dist[next] = cost;
+            pq.push({cost, next});
         }
     }
 }
 
 int main(){
-    int N, M, start, u, v, w;
-    cin >> N >> M >> start;
-    D.resize(N+1, INF), weight.resize(N + 1);
-    for(int i = 0; i < M; i++) cin >> u >> v >> w, weight[u].push_back({v, w});
-    foo(start);
-    for(int i = 1; i < D.size(); i++){
-        if(D[i] == INF) cout << "INF\n";
-        else cout << D[i] << '\n';
+    fill(dist, dist + MAX, INF);
+    cin >> V >> E >> start;
+    start--;
+    dist[start] = 0;
+
+    int u, v, w;
+    for(int i = 0; i < E; i++){
+        cin >> u >> v >> w;
+        graph[u - 1].push_back({v - 1, w});
     }
+
+    dijkstra(start);
+
+    for(int i = 0; i < V; i++){
+        if(dist[i] == INF) cout << "INF\n";
+        else cout << dist[i] << '\n';
+    }
+
 }
